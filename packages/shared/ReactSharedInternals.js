daff1e5426cf407f6fff8000dd54a1e7d1339a5f
gaopeng0108 <gaopeng19960108@gmail.com>
2022-12-31 12:03:48 +0800

    :sparkles: fix react error

--- packages/shared/ReactSharedInternals.js
+++ packages/shared/ReactSharedInternals.js
@@ -7,9 +7,7 @@
  * @flow
  */
 
-import * as React from 'react';
+import ReactSharedInternals from '../react/src/ReactSharedInternals';
 
-const ReactSharedInternals =
-  React.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED;
 
 export default ReactSharedInternals;
